cdd653b253a95458f1ba39d25aa74a64314b9c3e
andixlm <andixev@gmail.com>
2018-03-27 00:23:31 +0300

    utils: Implement method to read points from file

---
+++ include/utils.hpp
@@ -0,0 +1,14 @@
+#ifndef UTILS_HPP
+#define UTILS_HPP
+
+#include <vector>
+#include <string>
+
+namespace Utils
+{
+
+std::vector<double> readPoints(std::string fileName);
+
+}
+
+#endif // UTILS_HPP
---
+++ src/utils.cpp
@@ -0,0 +1,24 @@
+#include <fstream>
+#include <stdexcept>
+
+#include <utils.hpp>
+
+std::vector<double> Utils::readPoints(std::string fileName)
+{
+    std::ifstream input;
+    input.open(fileName);
+    if (!input.is_open())
+    {
+        throw std::runtime_error("Can't open file " + fileName);
+    }
+
+    double value = 0.0;
+    std::vector<double> result;
+    // TODO: Check for reading error.
+    while (input >> value)
+    {
+        result.push_back(value);
+    }
+
+    return result;
+}
