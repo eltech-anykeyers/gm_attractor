bb3e3aac7c7d1233bff33df4f70278703a36bf13
andixlm <andixev@gmail.com>
2018-03-29 21:14:07 +0300

    app: Run AttractorGLApp

--- src/main.cpp
+++ src/main.cpp
@@ -1,30 +1,9 @@
-#include <GL/gl.h>
-#include <GLFW/glfw3.h>
+#include <attractorglapp.hpp>
 
 int main(int argc, const char** argv)
 {
-    GLFWwindow* window;
+    AttractorGLApp app(640, 480, "The Coullet Attractor");
+    app.run();
 
-    if (!glfwInit())
-        return -1;
-
-    window = glfwCreateWindow(640, 480, "The Coullet Attractor", nullptr, nullptr);
-    if (!window)
-    {
-        glfwTerminate();
-        return -1;
-    }
-
-    glfwMakeContextCurrent(window);
-
-    while (!glfwWindowShouldClose(window))
-    {
-        glClear(GL_COLOR_BUFFER_BIT);
-
-        glfwSwapBuffers(window);
-        glfwPollEvents();
-    }
-
-    glfwTerminate();
     return 0;
 }
