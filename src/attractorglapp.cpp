6937ed561f2a59ebfc16bc6e1a9fd4628928febb
andixlm <andixev@gmail.com>
2018-04-01 16:38:01 +0300

    attractorglapp: Clear depth buffer

--- src/attractorglapp.cpp
+++ src/attractorglapp.cpp
@@ -4,7 +4,7 @@ void AttractorGLApp::mainLoop()
 {
     while (!glfwWindowShouldClose(mWindow))
     {
-        glClear(GL_COLOR_BUFFER_BIT);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
         glfwSwapBuffers(mWindow);
         glfwPollEvents();
