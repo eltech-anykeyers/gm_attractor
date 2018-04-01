28f47b2b9158867bb382e50d0cff503f0781ec40
andixlm <andixev@gmail.com>
2018-04-01 17:13:40 +0300

    shader: Fix typos

--- include/shader.hpp
+++ include/shader.hpp
@@ -103,53 +103,53 @@ public:
 
     void setVec2(const std::string& name, const glm::vec2& value) const
     {
-        glUniform2fv(glGetUniformLocation(ID, name.c_str()),
+        glUniform2fv(glGetUniformLocation(mID, name.c_str()),
                      1, &value[0]);
     }
     void setVec2(const std::string& name, float x, float y) const
     {
-        glUniform2f(glGetUniformLocation(ID, name.c_str()),
+        glUniform2f(glGetUniformLocation(mID, name.c_str()),
                     x, y);
     }
 
     void setVec3(const std::string& name, const glm::vec3& value) const
     {
-        glUniform3fv(glGetUniformLocation(ID, name.c_str()),
+        glUniform3fv(glGetUniformLocation(mID, name.c_str()),
                      1, &value[0]);
     }
     void setVec3(const std::string& name, float x, float y, float z) const
     {
-        glUniform3f(glGetUniformLocation(ID, name.c_str()),
+        glUniform3f(glGetUniformLocation(mID, name.c_str()),
                     x, y, z);
     }
 
     void setVec4(const std::string& name, const glm::vec4& value) const
     {
-        glUniform4fv(glGetUniformLocation(ID, name.c_str()),
+        glUniform4fv(glGetUniformLocation(mID, name.c_str()),
                      1, &value[0]);
     }
 
     void setVec4(const std::string& name, float x, float y, float z, float w) const
     {
-        glUniform4f(glGetUniformLocation(ID, name.c_str()),
+        glUniform4f(glGetUniformLocation(mID, name.c_str()),
                     x, y, z, w);
     }
 
     void setMat2(const std::string& name, const glm::mat2& mat) const
     {
-        glUniformMatrix2fv(glGetUniformLocation(ID, name.c_str()),
+        glUniformMatrix2fv(glGetUniformLocation(mID, name.c_str()),
                            1, GL_FALSE, &mat[0][0]);
     }
 
     void setMat3(const std::string& name, const glm::mat3& mat) const
     {
-        glUniformMatrix3fv(glGetUniformLocation(ID, name.c_str()),
+        glUniformMatrix3fv(glGetUniformLocation(mID, name.c_str()),
                            1, GL_FALSE, &mat[0][0]);
     }
 
     void setMat4(const std::string& name, const glm::mat4& mat) const
     {
-        glUniformMatrix4fv(glGetUniformLocation(ID, name.c_str()),
+        glUniformMatrix4fv(glGetUniformLocation(mID, name.c_str()),
                            1, GL_FALSE, &mat[0][0]);
     }
 
