36d428e32702b9fea2f3dd56e46b9e94b647a801
Deligor6321 <deligor6321@gmail.com>
2018-05-05 02:08:28 +0300

    main: Fix window title

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,7 @@
 
 int main(int argc, const char** argv)
 {
-    AttractorGLApp app(640, 480, "The Coullet Attractor");
+    AttractorGLApp app(640, 480, "Attractor Viewer");
     app.run();
 
     return 0;
