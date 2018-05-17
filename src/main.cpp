e1d091a290ed35544a2dff73e4774eb6d89d8141
andixlm <andixev@gmail.com>
2018-05-18 02:19:47 +0300

    app: Handle input args in a dumb way

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,15 @@
 int main(int argc, const char** argv)
 {
     AttractorGLApp app(640, 480, "Attractor Viewer");
+
+    if (argc == 5) /// Four args to define attractors trajectories and sections.
+    {
+        app.setFirstAttractorTrajectory  (argv[1]);
+        app.setFirstAttractorSection     (argv[2]);
+        app.setSecondAttractorTrajectory (argv[3]);
+        app.setSecondAttractorSection    (argv[4]);
+    }
+
     app.run();
 
     return 0;
