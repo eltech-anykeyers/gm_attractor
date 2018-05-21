23bf9ff4a4b5d0f0b187dfd68154c13f34caae47
andixlm <andixev@gmail.com>
2018-05-21 20:03:39 +0300

    shaders: Make attractors ready for ambient lighting

--- shaders/attractor/frag.glsl
+++ shaders/attractor/frag.glsl
@@ -1,10 +1,14 @@
 #version 330 core
 
 uniform vec4 color;
+uniform vec4 light_color;
 
 out vec4 FragColor;
 
 void main()
 {
-    FragColor = color;
+    float ambient_strength = 0.1;
+    vec4 ambient = ambient_strength * light_color;
+
+    FragColor = vec4(ambient.rgb * color.rgb, color.a);
 }
