9dd2075865c901e80faa84356beceb6b63404119
andixlm <andixev@gmail.com>
2018-05-22 23:51:49 +0300

    shaders: Update attractor shader

--- shaders/attractor/frag.glsl
+++ shaders/attractor/frag.glsl
@@ -1,14 +1,26 @@
 #version 330 core
 
 uniform vec4 color;
-uniform vec4 light_color;
+
+in vec3 polygon_normal;
+in vec4 frag_position;
 
 out vec4 FragColor;
 
 void main()
 {
-    float ambient_strength = 0.1;
+    vec3 light_position = vec3(-1.0f, 2.7f, 3.14f);
+    vec4 light_color = vec4(1.0f, 1.0f, 1.0f, 1.0f);
+
+    float ambient_strength = 0.314;
     vec4 ambient = ambient_strength * light_color;
 
-    FragColor = vec4(ambient.rgb * color.rgb, color.a);
+    vec3 normal = normalize(polygon_normal);
+    vec3 light_dir = normalize(light_position.xyz - frag_position.xyz);
+
+    float diff = max(dot(normal, light_dir), 0.0f);
+    vec4 diffuse = diff * light_color;
+
+    vec3 result = (ambient.rgb + diffuse.rgb) * color.rgb;
+    FragColor = vec4(result, color.a);
 }
--- shaders/attractor/vert.glsl
+++ shaders/attractor/vert.glsl
@@ -1,14 +1,28 @@
 #version 330 core
 
 layout (location = 0) in vec3 pos;
+layout (location = 1) in vec3 _polygon_normal;
+
+uniform vec4 model_0;
+uniform vec4 model_1;
+uniform vec4 model_2;
+uniform vec4 model_3;
 
 uniform vec4 trans_0;
 uniform vec4 trans_1;
 uniform vec4 trans_2;
 uniform vec4 trans_3;
 
+out vec4 frag_position;
+out vec3 polygon_normal;
+
 void main()
 {
+    polygon_normal = _polygon_normal;
+
+    mat4 model = mat4(model_0, model_1, model_2, model_3);
+    frag_position = vec4(model * vec4(pos, 1.0));
+
     mat4 transform = mat4(trans_0, trans_1, trans_2, trans_3);
     gl_Position = transform * vec4(pos, 1.0f);
 }
