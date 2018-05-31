5372a3b8ecb11ef978f9747ecd9295c11cd9bb10
andixlm <andixev@gmail.com>
2018-05-31 07:54:06 +0300

    Revert "attractorglapp: Disable attractors switching"
    This reverts commit 2950953dc95189e4d3be0507ee5b99ce45a5d622.

--- src/attractorglapp.cpp
+++ src/attractorglapp.cpp
@@ -170,6 +170,14 @@ void AttractorGLApp::processInput()
 
 void AttractorGLApp::processInputForAttractors()
 {
+    /// Attractors selection.
+    if (glfwGetKey(mWindow, GLFW_KEY_1) == GLFW_PRESS)
+        mAttractorFilter = AttractorFilter::FIRST;
+    if (glfwGetKey(mWindow, GLFW_KEY_2) == GLFW_PRESS)
+        mAttractorFilter = AttractorFilter::SECOND;
+    if (glfwGetKey(mWindow, GLFW_KEY_3) == GLFW_PRESS)
+        mAttractorFilter = AttractorFilter::BOTH;
+
     /// Attractors time.
     if (glfwGetKey(mWindow, GLFW_KEY_Q) == GLFW_PRESS)
         adjustAttractorTime(true);
